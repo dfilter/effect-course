69a37cce81d4fa87627cbbc0deb6dfd94696bfe4
dirk <dirk@maplelane.ag>
2025-04-01 16:12:28 -0400

    schema class

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,33 @@
 import { Data, Effect, Schema } from "effect";
 
-class FetchError extends Data.TaggedError("FetchError")<{}> {}
-
-class JsonError extends Data.TaggedError("JsonError")<{}> {}
-
-const Pokemon = Schema.Struct({
+/** Schema definition **/
+class Pokemon extends Schema.Class<Pokemon>("Pokemon")({
   id: Schema.Number,
   order: Schema.Number,
   name: Schema.String,
   height: Schema.Number,
   weight: Schema.Number,
-});
+}) {
+  /** We can define our own methods in here. **/
+}
 
+/** Errors **/
+class FetchError extends Data.TaggedError("FetchError")<{}> {}
+class JsonError extends Data.TaggedError("JsonError")<{}> {}
+
+/** Implementation **/
 const fetchRequest = Effect.tryPromise({
-  try: () => {
-    if (Math.random() < 0.25) {
-      throw new Error();
-    }
-    return fetch("https://pokeapi.co/api/v2/pokemon/garchomp/");
-  },
+  try: () => fetch("https://pokeapi.co/api/v2/pokemon/garchomp/"),
   catch: () => new FetchError(),
 });
 
 const jsonResponse = (response: Response) =>
   Effect.tryPromise({
-    try: () => {
-      if (Math.random() < 0.25) {
-        throw new Error();
-      }
-      return response.json();
-    },
+    try: () => response.json(),
     catch: () => new JsonError(),
   });
 
-const savePokemon = (pokemon: unknown) =>
-  Effect.tryPromise(
-    () => new Promise((resolve) => setTimeout(() => resolve(pokemon), 500))
-  );
+const decodePokemon = Schema.decodeUnknown(Pokemon);
 
 const program = Effect.gen(function* () {
   const response = yield* fetchRequest;
@@ -44,16 +35,19 @@ const program = Effect.gen(function* () {
     return yield* new FetchError();
   }
 
-  return yield* jsonResponse(response);
+  const json = yield* jsonResponse(response);
+
+  return yield* decodePokemon(json);
 });
 
+/** Error handling **/
 const main = program.pipe(
   Effect.catchTags({
     FetchError: () => Effect.succeed("Fetch error"),
     JsonError: () => Effect.succeed("Json error"),
+    ParseError: () => Effect.succeed("Parse error"),
   })
 );
 
-Effect.runPromise(main)
-  .then((json) => console.log(json))
-  .catch((error) => console.error(error));
+/** Running effect **/
+Effect.runPromise(main).then(console.log);
