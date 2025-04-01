b561a4df3a8ee3fa90553777a5d76f7e98f05a81
dirk <dirk@maplelane.ag>
2025-04-01 14:46:48 -0400

    catchTag

--- src/index.ts
+++ src/index.ts
@@ -5,37 +5,27 @@ const fetchRequest = Effect.tryPromise(() =>
 );
 
 const jsonResponse = (response: Response) =>
-  Effect.tryPromise(() => response.json());
+  Effect.tryPromise(() => {
+    if (Math.random() < 0.5) {
+      throw new Error();
+    } else {
+      return response.json();
+    }
+  });
 
 const savePokemon = (pokemon: unknown) =>
   Effect.tryPromise(
     () => new Promise((resolve) => setTimeout(() => resolve(pokemon), 500))
   );
 
-/*
-const main = pipe(
-  fetchRequest,
-  Effect.flatMap(jsonResponse), // <- pipe allows flatmap to pass the result of the previous effect into the next.
-  Effect.flatMap(savePokemon)
-);
-
-// A more verbose version of the above would be the following:
-const main = pipe(
-  fetchRequest,
-  (fetchRequestEffect) => Effect.flatMap(fetchRequestEffect, jsonResponse),
-  (jsonResponseEffect) => Effect.flatMap(jsonResponseEffect, savePokemon)
-);
-
-// You can also compose the above (without savePokemon) like so:
-const main = fetchRequest.pipe(
-  (fetchRequestEffect) => Effect.flatMap(jsonResponse)(fetchRequestEffect)
-);
-*/
-
-// Every effect has its own pipe function so we can also do something like this:
 const main = fetchRequest.pipe(
   Effect.flatMap(jsonResponse),
-  Effect.flatMap(savePokemon)
+  // Allows catching of a SINGLE error
+  Effect.catchTag("UnknownException", () =>
+    Effect.succeed<string>("There was an error")
+  )
 );
 
-Effect.runPromise(main).then((json) => console.log(json));
+Effect.runPromise(main)
+  .then((json) => console.log(json))
+  .catch((thing) => console.log(thing));
