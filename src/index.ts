3f1bf4cfeee0178e73c6af60c674f9d6acd4e41f
dirk <dirk@maplelane.ag>
2025-04-01 14:27:36 -0400

    the pipe function and method

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Effect } from "effect";
+import { Effect, pipe } from "effect";
 
 const fetchRequest = Effect.tryPromise(() =>
   fetch("https://pokeapi.co/api/v2/pokemon/garchomp/")
@@ -7,6 +7,35 @@ const fetchRequest = Effect.tryPromise(() =>
 const jsonResponse = (response: Response) =>
   Effect.tryPromise(() => response.json());
 
-const main = Effect.flatMap(fetchRequest, jsonResponse);
+const savePokemon = (pokemon: unknown) =>
+  Effect.tryPromise(
+    () => new Promise((resolve) => setTimeout(() => resolve(pokemon), 500))
+  );
+
+/*
+const main = pipe(
+  fetchRequest,
+  Effect.flatMap(jsonResponse), // <- pipe allows flatmap to pass the result of the previous effect into the next.
+  Effect.flatMap(savePokemon)
+);
+
+// A more verbose version of the above would be the following:
+const main = pipe(
+  fetchRequest,
+  (fetchRequestEffect) => Effect.flatMap(fetchRequestEffect, jsonResponse),
+  (jsonResponseEffect) => Effect.flatMap(jsonResponseEffect, savePokemon)
+);
+
+// You can also compose the above (without savePokemon) like so:
+const main = fetchRequest.pipe(
+  (fetchRequestEffect) => Effect.flatMap(jsonResponse)(fetchRequestEffect)
+);
+*/
+
+// Every effect has its own pipe function so we can also do something like this:
+const main = fetchRequest.pipe(
+  Effect.flatMap(jsonResponse),
+  Effect.flatMap(savePokemon)
+);
 
 Effect.runPromise(main).then((json) => console.log(json));
