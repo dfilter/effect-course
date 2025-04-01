ce113511dfae62adf9da900773fe372338a2373e
dirk <dirk@maplelane.ag>
2025-04-01 16:22:48 -0400

    Config

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,28 @@
-import { Data, Effect, Schema } from "effect";
+import { Schema } from "effect";
+import { Config, Data, Effect } from "effect";
 
-/** Schema definition **/
+/** Schema **/
 class Pokemon extends Schema.Class<Pokemon>("Pokemon")({
   id: Schema.Number,
   order: Schema.Number,
   name: Schema.String,
   height: Schema.Number,
   weight: Schema.Number,
-}) {
-  /** We can define our own methods in here. **/
-}
+}) {}
 
 /** Errors **/
 class FetchError extends Data.TaggedError("FetchError")<{}> {}
 class JsonError extends Data.TaggedError("JsonError")<{}> {}
 
+/** Configuration **/
+const config = Config.string("BASE_URL");
+
 /** Implementation **/
-const fetchRequest = Effect.tryPromise({
-  try: () => fetch("https://pokeapi.co/api/v2/pokemon/garchomp/"),
-  catch: () => new FetchError(),
-});
+const fetchRequest = (baseUrl: string) =>
+  Effect.tryPromise({
+    try: () => fetch(`${baseUrl}/api/v2/pokemon/garchomp/`),
+    catch: () => new FetchError(),
+  });
 
 const jsonResponse = (response: Response) =>
   Effect.tryPromise({
@@ -30,7 +33,8 @@ const jsonResponse = (response: Response) =>
 const decodePokemon = Schema.decodeUnknown(Pokemon);
 
 const program = Effect.gen(function* () {
-  const response = yield* fetchRequest;
+  const baseUrl = yield* config;
+  const response = yield* fetchRequest(baseUrl);
   if (!response.ok) {
     return yield* new FetchError();
   }
