61027bb80e44bb34b62772553ed71ac7bb5f50eb
dirk <dirk@maplelane.ag>
2025-04-01 16:26:46 -0400

    composability

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,30 @@
 import { Schema } from "effect";
-import { Config, Data, Effect } from "effect";
+import { Config, Effect } from "effect";
+import { FetchError, JsonError } from "./errors";
+import { Pokemon } from "./schemas";
 
-/** Schema **/
-class Pokemon extends Schema.Class<Pokemon>("Pokemon")({
-  id: Schema.Number,
-  order: Schema.Number,
-  name: Schema.String,
-  height: Schema.Number,
-  weight: Schema.Number,
-}) {}
+const getPokemon = Effect.gen(function* () {
+  const baseUrl = yield* Config.string("BASE_URL");
 
-/** Errors **/
-class FetchError extends Data.TaggedError("FetchError")<{}> {}
-class JsonError extends Data.TaggedError("JsonError")<{}> {}
-
-/** Configuration **/
-const config = Config.string("BASE_URL");
-
-/** Implementation **/
-const fetchRequest = (baseUrl: string) =>
-  Effect.tryPromise({
+  const response = yield* Effect.tryPromise({
     try: () => fetch(`${baseUrl}/api/v2/pokemon/garchomp/`),
     catch: () => new FetchError(),
   });
 
-const jsonResponse = (response: Response) =>
-  Effect.tryPromise({
-    try: () => response.json(),
-    catch: () => new JsonError(),
-  });
-
-const decodePokemon = Schema.decodeUnknown(Pokemon);
-
-const program = Effect.gen(function* () {
-  const baseUrl = yield* config;
-  const response = yield* fetchRequest(baseUrl);
   if (!response.ok) {
     return yield* new FetchError();
   }
 
-  const json = yield* jsonResponse(response);
+  const json = yield* Effect.tryPromise({
+    try: () => response.json(),
+    catch: () => new JsonError(),
+  });
 
-  return yield* decodePokemon(json);
+  return yield* Schema.decodeUnknown(Pokemon)(json);
 });
 
 /** Error handling **/
-const main = program.pipe(
+const main = getPokemon.pipe(
   Effect.catchTags({
     FetchError: () => Effect.succeed("Fetch error"),
     JsonError: () => Effect.succeed("Json error"),
