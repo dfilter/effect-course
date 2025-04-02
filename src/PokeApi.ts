e45a5d0fcea3c130155ddf9786d627cc5076dc19
dirk <dirk@maplelane.ag>
2025-04-02 08:29:06 -0400

    Using Context.Tag

--- src/PokeApi.ts
+++ src/PokeApi.ts
@@ -1,46 +1,41 @@
-import { Effect, Context, type ParseResult, Config, Schema } from "effect";
+import { Config, Context, Effect, Schema, type ParseResult } from "effect";
 import type { ConfigError } from "effect/ConfigError";
 import { FetchError, JsonError } from "./errors";
 import { Pokemon } from "./schemas";
 
-export interface PokeApi {
+interface PokeApiImpl {
   readonly getPokemon: Effect.Effect<
     Pokemon,
     FetchError | JsonError | ParseResult.ParseError | ConfigError
   >;
 }
 
-export const PokeApi = Context.GenericTag<PokeApi>("PokeApi");
+export class PokeApi extends Context.Tag("PokeApi")<PokeApi, PokeApiImpl>() {
+  static readonly Live = PokeApi.of({
+    getPokemon: Effect.gen(function* () {
+      const baseUrl = yield* Config.string("BASE_URL");
 
-// 👉 `PokeApi.of` defines a concrete implementation for the service
-export const PokeApiLive = PokeApi.of({
-  getPokemon: Effect.gen(function* () {
-    const baseUrl = yield* Config.string("BASE_URL");
+      const response = yield* Effect.tryPromise({
+        try: () => fetch(`${baseUrl}/api/v2/pokemon/garchomp/`),
+        catch: () => new FetchError(),
+      });
 
-    const response = yield* Effect.tryPromise({
-      try: () => fetch(`${baseUrl}/api/v2/pokemon/garchomp/`),
-      catch: () => new FetchError(),
-    });
+      if (!response.ok) {
+        return yield* new FetchError();
+      }
 
-    if (!response.ok) {
-      return yield* new FetchError();
-    }
+      const json = yield* Effect.tryPromise({
+        try: () => response.json(),
+        catch: () => new JsonError(),
+      });
 
-    const json = yield* Effect.tryPromise({
-      try: () => response.json(),
-      catch: () => new JsonError(),
-    });
+      return yield* Schema.decodeUnknown(Pokemon)(json);
+    }),
+  });
 
-    return yield* Schema.decodeUnknown(Pokemon)(json);
-  }),
-});
-
-export const PokeApiTest = PokeApi.of({
-  getPokemon: Effect.succeed({
-    id: 1,
-    height: 10,
-    weight: 10,
-    order: 1,
-    name: "myname",
-  }),
-});
+  static readonly Test = PokeApi.of({
+    getPokemon: Effect.succeed(
+      new Pokemon({ id: 1, order: 1, name: "myname", height: 10, weight: 10 })
+    ),
+  });
+}
